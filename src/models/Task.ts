9bf28435806446122e081c6e8c6ca2791303f622
kader009 <kadermollarasel000@gmail.com>
2024-12-29 21:46:12 +0600

    user required for task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -11,7 +11,7 @@ const TaskScheme: Schema = new Schema(
     name: { type: String, required: true },
     description: { type: String, required: true },
     isActive: { type: Boolean, required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'user' },
+    user: { type: Schema.Types.ObjectId, ref: 'User', unique: true, required: [true, 'user id is required'] },
   },
   {
     timestamps: true,
