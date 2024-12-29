32b3bfb701faeb8d0ad4de1e267c8516df405315
kader009 <kadermollarasel000@gmail.com>
2024-12-29 20:26:54 +0600

    add user info into the task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -6,13 +6,17 @@ interface taskType {
   isActive: boolean;
 }
 
-const TaskScheme: Schema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  isActive: { type: Boolean, required: true },
-},{
-  timestamps: true,
-});
+const TaskScheme: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    isActive: { type: Boolean, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'user' },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Tasks = mongoose.model<taskType>('task', TaskScheme);
 
