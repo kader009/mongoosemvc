b20a764e742db1c730d2abe6d84cada6cc33a39b
kader009 <kadermollarasel000@gmail.com>
2025-02-16 13:31:38 +0600

    modify user router

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import { UserController } from '../controller/UserController';
-import { UserValidation } from '../validations/UserValidation';
-import { validate } from '../middleware/validationMiddleware';
 
 const router = Router();
 
-router.get('/', UserController.getUsers);
-router.post('/register', validate(UserValidation), UserController.createUser);
-router.post('/login', UserController.loginUsers);
+router.get('/');
+router.post('/register');
 
 export default router;
