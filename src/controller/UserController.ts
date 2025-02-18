9086de42fa5dc73ec6fe990f388d7f07edbc3681
kader009 <kadermollarasel000@gmail.com>
2025-02-18 10:46:41 +0600

    structure user get methods

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,6 @@
 import { Request, Response } from 'express';
 import User from '../models/UserModel';
 
-const getUser = async (req: Request, res: Response) => {
-  try {
-    const users = await User.findById(req.params.id).select('-password');
-
-    if (!users) {
-      const error = new Error('User not found') as Error & { status: number };
-      error.status = 404;
-      throw error;
-    }
-
-    res.status(200).json({ success: true, data: users });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: 'Failed to create user',
-      error: (error as Error).message,
-    });
-  }
-};
-
 const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
     const user = await User.find();
@@ -32,6 +12,29 @@ const getUsers = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({
       success: false,
       message: 'Failed to fetch users',
+      error: (error as Error).message,
+    });
+  }
+};
+
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const users = await User.findById(req.params.id).select('-password');
+
+    if (!users) {
+      const error = new Error('User not found') as Error & { status: number };
+      error.status = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Failed to create user',
       error: (error as Error).message,
     });
   }
--- src/validations/UserValidation.ts
+++ src/validations/UserValidation.ts
@@ -4,5 +4,5 @@ export const UserValidation = z.object({
   name: z.string().min(8, 'Name must be at least 8 characters'),
   email: z.string().email('Invalid email format'),
   password: z.string().min(8, 'Password must be at least 8 characters'),
-  role: z.enum(['user', 'admin', 'manager']).default('user'),
+  role: z.enum(['user', 'admin']).default('user'),
 });
